13a69dc1f2a906f2a3481fecc23d240ec2b6a4ab
VYEDID <vyedid@frba.utn.edu.ar>
2024-04-29 16:31:48 -0300

    empezamos codigo

---
+++ src/main/java/ar/edu/utn/frba/dds/domain/reaccion/EsperarFalsaAlarma.java
@@ -0,0 +1,10 @@
+package ar.edu.utn.frba.dds.domain.reaccion;
+
+public class EsperarFalsaAlarma implements Reaccion{
+
+    private Integer minutos;
+
+    public void reaccionar(){
+
+    }
+}
---
+++ src/main/java/ar/edu/utn/frba/dds/domain/reaccion/RealizarLlamada.java
@@ -0,0 +1,10 @@
+package ar.edu.utn.frba.dds.domain.reaccion;
+
+public class RealizarLlamada implements Reaccion{
+
+    private Integer numero;
+
+    public void reaccionar(){
+
+    }
+}
